c6fc7713348add482c49e2aa153ab2f7341867aa
LesterLyu <lvds2000@gmail.com>
2021-01-02 01:29:01 -0500

    oneSidedDifferenceOfProportions and twoSidedDifferenceOfProportions are static functions now.

--- test/test/differenceOfProportions-test.js
+++ test/test/differenceOfProportions-test.js
@@ -10,20 +10,20 @@ suite.addBatch({
       return jStat;
     },
     'finds the one sided difference of proportions': function(jStat) {
-      var p = jStat.fn.oneSidedDifferenceOfProportions(.12, 500, .14, 1000);
+      var p = jStat.oneSidedDifferenceOfProportions(.12, 500, .14, 1000);
       assert.isTrue(p > 0.14);
       assert.isTrue(p < 0.15);
 
-      p = jStat.fn.oneSidedDifferenceOfProportions(.5, 134, .51, 101);
+      p = jStat.oneSidedDifferenceOfProportions(.5, 134, .51, 101);
       assert.isTrue(p > 0.43);
       assert.isTrue(p < 0.44);
     },
     'finds the two sided difference of proportions': function(jStat) {
-      var p = jStat.fn.twoSidedDifferenceOfProportions(.12, 500, .14, 1000);
+      var p = jStat.twoSidedDifferenceOfProportions(.12, 500, .14, 1000);
       assert.isTrue(p > 0.28);
       assert.isTrue(p < 0.29);
 
-      p = jStat.fn.twoSidedDifferenceOfProportions(.5, 134, .51, 101);
+      p = jStat.twoSidedDifferenceOfProportions(.5, 134, .51, 101);
       assert.isTrue(p > 0.87);
       assert.isTrue(p < 0.88);
     }
